9f95226955dd3d286e1bd7cd10a443d0197b053c
boa85 <belaev.oa85@gmail.com>
2017-12-01 21:28:21 +0600

    added template for doxygen comments

--- src/xbridge/util/logger.h
+++ src/xbridge/util/logger.h
@@ -19,21 +19,46 @@
 
 //******************************************************************************
 //******************************************************************************
+/**
+ * @brief The LOG class
+ */
 class LOG : public std::basic_stringstream<char, std::char_traits<char>,
                                         boost::pool_allocator<char> > // std::stringstream
 {
 public:
+    /**
+     * @brief LOG
+     * @param reason
+     */
     LOG(const char reason = 'I');
+
+    /**
+     * @brief ~LOG
+     */
     virtual ~LOG();
 
+    /**
+     * @brief logFileName
+     * @return
+     */
     static std::string logFileName();
 
 private:
+    /**
+     * @brief makeFileName
+     * @return
+     */
     static std::string makeFileName();
 
 private:
+    /**
+     * @brief m_r
+     */
     char m_r;
 
+    /**
+     * @brief m_logFileName
+     */
     static std::string m_logFileName;
 };
 
